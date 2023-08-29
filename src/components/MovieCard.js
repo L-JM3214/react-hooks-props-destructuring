54454eb9420594446908e4fc886176f6908911bb
Lactor <lactorjm4@gmail.com>
2023-08-29 23:01:16 +0300

    Completed Assignement

--- src/components/MovieCard.js
+++ src/components/MovieCard.js
@@ -1,5 +1,4 @@
 import React from "react";
-
 function MovieCard({
   title,
   posterSrc = "https://m.media-amazon.com/images/M/MV5BOTJjNzczMTUtNzc5MC00ODk0LWEwYjgtNzdiOTEyZmQxNzhmXkEyXkFqcGdeQXVyNzMzMjU5NDY@._V1_UY268_CR1,0,182,268_AL_.jpg",
@@ -13,5 +12,4 @@ function MovieCard({
     </div>
   );
 }
-
 export default MovieCard;
